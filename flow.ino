31cb74d7f3ef698ae8c2258af5578cebce632946
netanbi <32709540+netanbi@users.noreply.github.com>
2019-03-05 23:23:12 +0200

    Add files via upload

---
+++ flow.ino
@@ -0,0 +1,55 @@
+
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(7, 8, 9, 10, 11, 13);
+float sensorValue = 0; // The sensor value
+int tempValue;
+float sensorMin = 80;  // Minimum sensor value
+float sensorMax = 716;// Maximum sensor value
+float outMin = 7;
+float outMax = 150;
+
+int x;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  lcd.begin(20, 4);
+}
+float fmap (float sensorValue, float sensorMin, float sensorMax, float outMin, float outMax)
+    {
+  return (sensorValue - sensorMin) * (outMax - outMin) / (sensorMax - sensorMin) + outMin;
+     }
+void loop() {
+
+  sensorValue = analogRead(A7); 
+ 
+  sensorValue = fmap (sensorValue, sensorMin, sensorMax, 0, 150); 
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("Flow: ");
+  if (sensorValue >= 0)
+  {
+    lcd.print(sensorValue);
+  }
+  else
+  { 
+    lcd.print("0");  
+  }
+  lcd.print("L/min");
+  x = analogRead(A7);
+  Serial.println(x);
+  tempValue = analogRead(A4);
+  lcd.setCursor(0, 2);  
+  lcd.print("Temp: ");
+  lcd.print(tempValue);
+  lcd.print(" ");
+  lcd.print(char(223));
+  lcd.print("C");
+  delay(2000);
+
+}
+
---
+++ wifi_relay.ino
@@ -0,0 +1,108 @@
+/*
+ * ESP8266 (WeMosD1) WiFi Relay Control
+ * 
+ * learnelectronics
+ * 05 JUN 2017
+ * 
+ * www.youtube.com/c/learnelectronics
+ * arduino0169@gmail.com
+*/
+
+
+#include <ESP8266WiFi.h>
+ 
+const char* ssid = "Phinergy_Qadima";
+const char* password = "Qadimaev2015";
+ 
+int ledPin_1 = 2;
+
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+ 
+ 
+  pinMode(ledPin_1, OUTPUT);
+  digitalWrite(ledPin_1, LOW);
+  
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL : ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+  int value_1 = LOW;
+  if (request.indexOf("/LED_1=OFF") != -1) {
+    digitalWrite(ledPin_1, LOW);
+    value_1 = HIGH;
+  }
+  if (request.indexOf("/LED_1=ON") != -1){
+    digitalWrite(ledPin_1, HIGH);
+    value_1 = LOW;
+  }
+  
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+ 
+  client.print("Led pin is now: ");
+ 
+  if(value_1 == LOW) {
+    client.print("OFF");  
+  } else {
+    client.print("ON");
+  }
+  client.println("<br><br>");
+  client.println("Click <a href=\"/LED_1=OFF\">here</a>  to turn relay ON<br>");
+  client.println("Click <a href=\"/LED_1=ON\">here</a>  to turn relay OFF<br>");
+  client.println("</html>");
+  delay(1);
+  Serial.println("Client disconnected");
+  Serial.println("");
+ 
+}
